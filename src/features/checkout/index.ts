f125baf2068c5e61fbe69f3f6c248fbfafadfc6d
Jesal Pande <pandejesal@gmail.com>
2026-09-11 10:05:51 +0530

    Edge loop: delete 4 dead checkout components
    TakeawayPanel, DineInPanel, NotesEditor, CheckoutSection: zero references outside barrel (superseded by FulfillmentDetailsCard). Barrel cleaned.

--- src/features/checkout/index.ts
+++ src/features/checkout/index.ts
@@ -1,10 +1,6 @@
 export * from "./repositories/CheckoutRepository";
 export * from "./services/checkoutService";
 export * from "./state/checkoutStore";
-export { CheckoutSection } from "./components/CheckoutSection";
-export { NotesEditor } from "./components/NotesEditor";
-export { TakeawayPanel } from "./components/TakeawayPanel";
-export { DineInPanel } from "./components/DineInPanel";
 export { ReviewItemsList } from "./components/ReviewItemsList";
 export { AddressSelector, calculateHaversineKm } from "./components/AddressSelector";
 export { FulfillmentDetailsCard } from "./components/FulfillmentDetailsCard";
